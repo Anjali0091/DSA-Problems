aaa8397586413250444ce78ee7312ef9d5d44213
Anjali <aryaanjalit.5@gmail.com>
2026-09-11 01:36:01 +0530

    Time: 3 ms (86.03%) | Memory: 43.3 MB (57.10%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,27 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
+import java.util.Stack;
 
-        for(char c: s.toCharArray()) {
-            if(c=='('||c=='{'||c=='[') {
-                stack.push(c);
-            }
-            else {
-                if(stack.isEmpty()) {
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
-                char top=stack.pop();
 
-                if(c==')' && top!='(' || 
-                c=='}' && top!='{' ||
-                c==']' && top!='[') {
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
                     return false;
                 }
             }
         }
+
         return stack.isEmpty();
     }
 }
